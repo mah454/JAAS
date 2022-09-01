43cae9014a289e91fcc9c60cae32391cd7d6964f
mah454 <mah454@gmail.com>
2022-09-01 12:50:57 +0430

    cleanup code

--- jaas-http-jwt/src/main/java/ir/moke/jaas/handler/HelloHandler.java
+++ jaas-http-jwt/src/main/java/ir/moke/jaas/handler/HelloHandler.java
@@ -3,8 +3,6 @@ package ir.moke.jaas.handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-
 public class HelloHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) {
